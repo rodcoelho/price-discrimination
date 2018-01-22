0b05b4b6f08f11003dc8e59ba3dd4f05fa43d473
rodcoelho <rodcpython@gmail.com>
2018-01-22 15:53:33 -0500

    fixed bug on curl -X GET command

--- build.py
+++ build.py
@@ -5,33 +5,38 @@ import json, os, re, sys, time, socket, requests
 def create_digital_ocean_vps():
     endpoint = 'https://api.digitalocean.com/v2/droplets'
     hostname = 'node'
-    api_data = {}
-    mac_pa_token = open('/Users/{username}/.pat/.digitalocean'.format(username=('rodrigocoelho'))).read() #FIXME
-    a_header = 'Authorization: Bearer {pa_token}'.format(pa_token=mac_pa_token)
+    payload = {}
+    pat_path = '/Users/{username}/.pat/.digitalocean'.format(username=('rodrigocoelho'))
+    mac_pa_token = open('{pat_path}'.format(pat_path=pat_path)).read().strip
+    a_header = 'Authorization: Bearer {mac_pa_token}'.format(mac_pa_token=mac_pa_token)
     c_header = 'Content-Type: application/json'
-    vm_count = int(input('How many VPSs do you want to spin up?'))
+    vm_count = int(input('How many VMs do you want to spin up?'))
     if vm_count < 1:
         print("ERROR: You cannot spin up less than one server")
         create_digital_ocean_vps()
-    elif vm_count <2:
-        api_data['name'] = hostname
+    elif vm_count < 2:
+        payload['name'] = hostname
     else:
-        api_data['names'] = ['{hostname} {_}'
-                                .format(hostname=hostname,
-                                        _=_)
-                                .replace(' ', '-')
-                                for _ in range(vm_count)]
-    api_data['region'] = 'nyc3'
-    api_data['size']   = '1gb'
-    api_data['image']  = 'ubuntu-16-04-x64'
-    api_data['ssh_keys'] = ['{ssh_key_id}'.format(ssh_key_id=17337718)] #FIXME
-    api_data['tags'] = ['test']
+        payload['names'] = ['{hostname} {_}'.format(hostname=hostname,_=_).replace(' ', '-')for _ in range(vm_count)]
+    payload['region'] = 'nyc1'
+    payload['size']   = '1gb'
+    payload['image']  = 'ubuntu-16-04-x64'
+    headers = {}
+    headers['Authorization'] = 'Bearer {mac_pa_token}'.format(mac_pa_token=mac_pa_token)
+    headers['Content-Type'] = 'application/json'
+    keys = json.loads(requests.get('https://api.digitalocean.com/v2/account/keys', headers=headers).text)['ssh_keys']
+    payload['ssh_keys'] = [str(key['id']) for key in keys if key['name'] == socket.gethostname()]
+    # payload['ssh_keys'] = ['{ssh_key_id}'.format(ssh_key_id=17337718)] #FIXME
+    payload['tags'] = ['test']
+    # FIXME might be causing issue
+    # FIXME might be causing issue
+    # FIXME might be causing issue BELOW
     endstate = 'curl -X POST "{endpoint}"              \
                 -d \'{api_data}\'                      \
                 -H "{a_header}"                        \
                 -H "{c_header}"'                       \
                 .format(endpoint=endpoint,
-                        api_data=json.dumps(api_data),
+                        api_data=json.dumps(payload),
                         a_header=a_header.strip(),
                         c_header=c_header)
     return re.sub(' +', ' ', endstate)
@@ -56,9 +61,9 @@ def build_single_vps():
 
 def get_host(droplet_id, writeout_file):
     pa_token = open('/Users/rodrigocoelho/.pat/.digitalocean').read()
+    #writeout_file_i = writeout_file.split('.')[0] + writeout_file.split('.')[1] + '-' + str(droplet_id) + '.json'
     writeout_file_i = writeout_file.split('.')[0] + writeout_file.split('.')[1] + '-' + str(droplet_id) + '.json'
-    print(writeout_file_i)
-    os.system('curl -X GET "https://api.digitalocean.com/v2/droplets/{droplet_id} -H "Content-Type: application/json" -H "Authorization: Bearer {pa_token}" > {writeout_file_i}'.format(droplet_id=droplet_id,pa_token=pa_token,writeout_file_i=writeout_file_i))
+    os.system('curl -X GET "https://api.digitalocean.com/v2/droplets/{droplet_id}" -H "Content-Type: application/json" -H "Authorization: Bearer {pa_token}" > {writeout_file_i}'.format(droplet_id=droplet_id,pa_token=pa_token,writeout_file_i=writeout_file_i))
     payload = json.load(open(writeout_file_i))
     ip_address = payload['droplet']['networks']['v4'][0]['ip_address']
     return ip_address
