921cd3d0070ee2868063b69de116ce0e00c4fa59
rodcoelho <rodcpython@gmail.com>
2018-01-23 22:57:02 -0500

    PEP

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+
 # builds droplet on digital ocean
-#python3 build.py
 python3 shell.py
 
+# to delete droplets by tags
 # curl -X DELETE -H "Content-Type: application/json" -H "Authorization: Bearer d902bef3397deb0a587ee028b75c587019790924046767a3227fb435bd01131e" "https://api.digitalocean.com/v2/droplets?tag_name=test"
